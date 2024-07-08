745185f5cf32b60361efea533d61bb1a2e81eeb8
Manmeet Kohli <manmeet.kohli@irisbusiness.com>
2024-07-08 23:03:31 +0530

    Initial setup

---
+++ config/settings/local.py
@@ -0,0 +1,16 @@
+from config.settings.base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+    }
+}
+
+DEBUG = str_to_bool(os.getenv('DEBUG'))
+
